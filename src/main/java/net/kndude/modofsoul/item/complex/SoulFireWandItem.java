69c81eca3c721d60a296ff4e004d20390fe6e9cd
Krishan Sachdev <krishansachdev@hotmail.co.uk>
2024-08-28 17:39:18 +0100

    added sound to usage of firewand and added basic small fireball for its projectile

--- src/main/java/net/kndude/modofsoul/item/complex/SoulFireWandItem.java
+++ src/main/java/net/kndude/modofsoul/item/complex/SoulFireWandItem.java
@@ -6,6 +6,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
@@ -13,6 +14,7 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.entity.projectile.SmallFireball;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -20,6 +22,7 @@ import net.minecraft.world.item.ProjectileItem;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.Properties;
 
@@ -45,8 +48,8 @@ public class SoulFireWandItem extends Item implements ProjectileItem {
         }
         else {
             player.startUsingItem(hand);
+            level.playSound(null, player.getOnPos(), SoundEvents.BLAZE_SHOOT, SoundSource.BLOCKS, 0.5f, 0.5f);
             if(!level.isClientSide()){
-                level.playSound(player, player.getOnPos(), SoundEvents.DRAGON_FIREBALL_EXPLODE, SoundSource.BLOCKS, 1f,1f);
                 itemstack.hurtAndBreak(1,((ServerLevel) level) , player, item -> player.onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
             }
             return InteractionResultHolder.consume(itemstack);
@@ -55,6 +58,13 @@ public class SoulFireWandItem extends Item implements ProjectileItem {
 
     @Override
     public Projectile asProjectile(Level level, Position pos, ItemStack stack, Direction direction) {
-        return null;
+        RandomSource randomsource = level.getRandom();
+        double d0 = randomsource.triangle((double)direction.getStepX(), 0.11485000000000001);
+        double d1 = randomsource.triangle((double)direction.getStepY(), 0.11485000000000001);
+        double d2 = randomsource.triangle((double)direction.getStepZ(), 0.11485000000000001);
+        Vec3 vec3 = new Vec3(d0, d1, d2);
+        SmallFireball smallfireball = new SmallFireball(level, pos.x(), pos.y(), pos.z(), vec3.normalize());
+        smallfireball.setItem(stack);
+        return smallfireball;
     }
 }
